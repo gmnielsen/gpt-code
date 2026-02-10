f0ad062f12927096492da976130317476979575e
Gary Nielsen <nielsen@cheverus.org>
2026-02-10 12:16:23 -0500

    added attribution

--- Robot.py
+++ Robot.py
@@ -1,3 +1,6 @@
+# this code was created by ChatGPT, an AI language model developed by OpenAI, under the instruction of Dr. Gary Nielsen
+
+
 import wpilib
 from RobotContainer import RobotContainer
 
